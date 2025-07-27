e3df5f02708bb8365dc7497864d0888b755b0cbf
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2025-07-27 19:47:08 +0100

    feat: integrate motion effects in AboutHomepage component for enhanced animations

--- src/components/about/AboutHomepage.tsx
+++ src/components/about/AboutHomepage.tsx
@@ -1,6 +1,7 @@
 import { NavDisplay } from "@/components/NavDisplay";
 import Image from "next/image"
-
+import {motion} from "framer-motion"
+import { zoomIn } from "../animations/motion";
 interface AboutHomepageProps {
   pathSegments: string[];
 }
@@ -9,21 +10,26 @@ export function AboutHomepage({ pathSegments }: AboutHomepageProps) {
   return (
     <div className="w-full">
       <header
-        className="w-full bg-cover bg-center bg-no-repeat h-[316px] hidden lg:block"
+        className="w-full bg-cover bg-center bg-no-repeat h-[316px]"
         style={{ backgroundImage: "url('/images/furniro_shop-hero-bg.svg')" }}
       >
-        <div className="flex flex-col gap-2 w-full h-full justify-center items-center">
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={zoomIn}
+          className="flex flex-col gap-2 w-full h-full justify-center items-center"
+        >
           <Image
             src="/images/furniro_path-logo.svg"
             alt="Furniro Logo"
             height={77}
             width={77}
           />
-          <h1 className="font-medium text-5xl capitalize">
+          <h1 className="font-medium lg:text-5xl text-3xl capitalize">
             {pathSegments[pathSegments.length - 1] || "Shop"}
           </h1>
           <NavDisplay pathSegments={pathSegments} />
-        </div>
+        </motion.div>
       </header>
     </div>
   );
